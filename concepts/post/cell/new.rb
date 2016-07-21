75de5305244cdbe36b018f25d00c0ce2ed741e98
Fran Worley <frances@safetytoolbox.co.uk>
2016-07-21 07:58:29 +0100

    use new formular helper

--- concepts/post/cell/new.rb
+++ concepts/post/cell/new.rb
@@ -1,20 +1,37 @@
 require "formular"
-require 'formular/builders/bootstrap3'
+#require 'formular/builders/bootstrap3'
 module Post::Cell
   class New < Trailblazer::Cell
-    def vertical_form(model:nil, **options, &block)
-      Formular::Builders::Bootstrap3.new(model: model, path: :comment).form(options, &block)
-    end
-
-    def inline_form(model:nil, **options, &block)
-      Formular::Builders::Bootstrap3Inline.new(model: model, path: :comment).form(options, &block)
-    end
-
-    def horizontal_form(model:nil, **options, &block)
-      Formular::Builders::Bootstrap3Horizontal.new(model: model, path: :comment).form(options, &block)
-    end
+    include Formular::Helper
 
   private
+    def vertical_form(model, url, **options, &block)
+      options[:builder] = :bootstrap3
+      form(model, url, options, &block)
+      #Formular::Builders::Bootstrap3.new(model: model, prefix_path: :comment).form(options, &block)
+    end
+
+    def inline_form(model, url, **options, &block)
+      options[:builder] = :bootstrap3_inline
+      form(model, url, options, &block)
+
+      #Formular::Builders::Bootstrap3Inline.new(model: model, prefix_path: :comment).form(options, &block)
+    end
+
+    def horizontal_form(model, url, **options, &block)
+      options[:builder] = :bootstrap3_horizontal
+      form(model, url, options, &block)
+      #Formular::Builders::Bootstrap3Horizontal.new(model: model, prefix_path: :comment).form(options, &block)
+    end
+
+    def options_array
+      [[1, "Option 1"], [2, "Option 2"], [3, "Option 3"]]
+    end
+
+    def roles_array
+      [[nil, 'Choose a role'], [1, "Admin"], [2, "Owner"], [3, "Maintainer"]]
+    end
+
     def url
       options[:url] || raise("no action URL!")
     end
